d9e28f71701bd2463e68bd3192a7d6cb92461497
Dnhem <danadevelops@gmail.com>
2025-12-15 17:33:13 +0100

    add posthog for analytics

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,6 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 import { store } from "./redux/store";
 import { Provider } from "react-redux";
 import { CookiesProvider } from "react-cookie";
+import { PostHogProvider } from "posthog-js/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -13,7 +14,17 @@ root.render(
     <CookiesProvider>
       <Provider store={store}>
         <BrowserRouter>
-          <App />
+          <PostHogProvider
+            apiKey={process.env.REACT_APP_POSTHOG_KEY}
+            options={{
+              api_host: process.env.REACT_APP_POSTHOG_HOST,
+              defaults: '2025-05-24',
+              capture_exceptions: true,
+              debug: process.env.NODE_ENV === "development",
+            }}
+          >
+            <App />
+          </PostHogProvider>
         </BrowserRouter>
       </Provider>
     </CookiesProvider>
